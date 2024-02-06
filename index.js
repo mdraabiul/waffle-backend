cf232f388ecb5bcea32be11c506421986f64a2fd
rabiul <mdrabiul.asia@gmail.com>
2024-02-06 16:43:46 +0600

    vercel setup codes

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ import router from "./appRoute.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      "https://vercel.com/md-rabiuls-projects/waffle-backend/5QKMi9BwdhzkQsY8qvDLnfTKGXd3",
+    methods: ["POST", "GET", "UPDATE", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(morgan("tiny"));
